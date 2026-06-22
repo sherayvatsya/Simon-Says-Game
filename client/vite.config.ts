21b7316ae9b8796a83a072cc0e9589c2edeca09d
sherayvatsya <sherayvatsya@users.noreply.github.com>
2026-06-22 22:44:09 +0530

    chore: compile and output client build to root directory for easy hosting

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,6 +5,10 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    outDir: '../',
+    emptyOutDir: false,
+  },
   server: {
     port: 5173,
     proxy: {
